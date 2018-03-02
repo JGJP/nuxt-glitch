0ebd814a982c8b43bc5cb95c04134cd3000922e8
J <j@jgarcia.jp>
2018-03-02 11:34:54 +0900

    initial

---
+++ layouts/default.vue
@@ -0,0 +1,57 @@
+<template lang="pug">
+
+.layout
+
+    header
+        h1 header
+        nuxt-link(to="/") home page
+        nuxt-link(to="/about/") about page
+
+    nuxt
+
+    footer
+        h1 footer
+
+</template>
+
+<style>
+
+/*.page-leave-active {
+    transition: opacity .1s ease-out, transform .1s ease-out;
+}
+.page-enter-active {
+    transition: opacity .3s ease-out, transform .3s ease-out;
+}
+
+.page-leave-active {
+    opacity: 0;
+    transform: translateY(.3em);
+}
+
+.page-enter {
+    opacity: 0;
+    transform: translateY(.5em);
+}*/
+
+header {
+    background: blue;
+    height: 10em;
+    color: white;
+}
+
+header a {
+    padding: 1em;
+}
+
+main {
+    background: yellow;
+    height: 20em;
+}
+
+footer {
+    background: red;
+    height: 10em;
+    color: white;
+}
+
+</style>
